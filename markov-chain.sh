036189ae033b2ced2fd66b5e3a57124786375b2d
Vít Novotný <witiko@gmail.com>
2016-05-20 17:49:19 +0200

    The shell script now runs out of the PATH.

--- markov-chain.sh
+++ markov-chain.sh
@@ -13,4 +13,4 @@ for ARG in "${@:7}"; do
 done
 
 # Execute the script.
-./markov-chain.lua "${@:1:6}" "${ARGS[@]}" | tee track.csv | csvmidi
+markov-chain.lua "${@:1:6}" "${ARGS[@]}" | tee track.csv | csvmidi
